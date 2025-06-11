8d9079f70add3b2b8072e0c0a275f3e74ded66e4
Michael <100490812+DebSol11@users.noreply.github.com>
2025-06-11 16:43:18 +0200

    Shift repetitive code in script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+var html = "";
+for (var i = 0, l = 4; l > i; i++) {
+    html += `<div class="inner-container">
+                <div class="placeholder-image"></div>
+                <p>this is some subtext under an illustration or image</p>
+            </div>`;
+}
+
+document.getElementsByClassName('information-container')[0].innerHTML = html;
