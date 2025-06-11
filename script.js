072444e0fe30286ac61231f64ed85a4c56b97925
Michael <100490812+DebSol11@users.noreply.github.com>
2025-06-11 18:55:26 +0200

    Try to change from innerHTML to innerText
    This proofs to be more code intensive than initially thought.
    Hopefully this is outweighed by something in the future.
    Who knows?

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var html = "";
-for (var i = 0, l = 4; l > i; i++) {
-    html += `<div class="inner-container">
-                <div class="placeholder-image"></div>
-                <p>this is some subtext under an illustration or image</p>
-            </div>`;
-}
+const dummyText = ['this is some subtext under an illustration or image','this is some subtext under an illustration or image','this is some subtext under an illustration or image','this is some subtext under an illustration or image'];
 
-document.getElementsByClassName('information-container').innerText = html;
+dummyText.forEach(item => {
+    const newDivContainer = document.createElement('div');
+    newDivContainer.classList.add("inner-container");
+    const newImageDiv = document.createElement('div');
+    newImageDiv.classList.add("placeholder-image");
+    const newDummyText = document.createElement('p');
+
+    const selectElement = document.getElementsByClassName('information-container');
+
+    selectElement.appendChild(newDivContainer);
+    newDivContainer.appendChild(newImageDiv);
+    newDivContainer.appendChild(newDummyText);
+
+    newDummyText.innerText = item;
+});
+
+// Solution with future security concerns 
+// -----------------------------
+// var html = "";
+// for (var i = 0, l = 4; l > i; i++) {
+//     html += `<div class="inner-container">
+//                 <div class="placeholder-image"></div>
+//                 <p>this is some subtext under an illustration or image</p>
+//             </div>`;
+// }
+
+// document.getElementsByClassName('information-container')[0].innerHTML = html;
